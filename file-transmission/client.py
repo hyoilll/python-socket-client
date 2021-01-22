adccc3387db61f35f75db4b3341eb0db1493c7ab
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-01-23 01:21:25 +0900

    server -> client end

--- file-transmission/client.py
+++ file-transmission/client.py
@@ -10,26 +10,41 @@ PORT = 8080
 clientSock.connect((HOST, PORT))
 print('연결성공')
 
-fileName = input('전송할 파일 이름을 입력하시오. : ')
-clientSock.send(fileName.encode('utf-8'))
+while True:
+    print('# 1 클라이언트 -> 서버 업로드')
+    print('# 2 서버 -> 클라이언트 다운로드')
+    print('# 3 서버 파일 리스트 조회')
+    print('menu -> ', end='')
+    menu = int(input())
 
-data = clientSock.recv(1024)
-data_transferred = 0
+    if menu == 1:  # 1 클라이언트 -> 서버 업로드
+        pass
+    elif menu == 2:  # 2 서버 -> 클라이언트 다운로드
+        fileName = input('전송할 파일 이름을 입력하시오. : ')
+        clientSock.send(fileName.encode('utf-8'))
 
-if not data:
-    print('파일 %s 가 서버에 존재하지 않음' % fileName)
-    sys.exit()
+        data = clientSock.recv(1024)
+        data_transferred = 0
 
-nowdir = os.getcwd()
-print(nowdir)
+        if not data:
+            print('파일 %s 가 서버에 존재하지 않음' % fileName)
+            sys.exit()
 
-with open(nowdir + "\\file-transmission\\" + fileName, "wb") as f:  # 현재 dir에 파일을 받음
-    try:
-        while data:
-            f.write(data)  # 1024byte 씀
-            data_transferred += len(data)
-            data = clientSock.recv(1024)
-    except Exception as ex:
-        print(ex)
+        nowdir = os.getcwd()
 
-print('수신완료 %s, 전송량 %d' % (fileName, data_transferred))
+        with open(nowdir + "\\file-transmission\\" + fileName, "wb") as f:  # 현재 dir에 파일을 받음
+            try:
+                while data:
+                    f.write(data)  # 1024byte 씀
+                    data_transferred += len(data)
+                    data = clientSock.recv(1024)
+            except Exception as ex:
+                print(ex)
+
+        print('수신완료 %s, 전송량 %d' % (fileName, data_transferred))
+
+        break
+    elif menu == 3:  # 3 서버 파일 리스트 조회
+        pass
+    else:
+        print('1 ~ 3의 범위에서 선택해주세요.')
