e62de4254a052538d7ff61a1e5cb63374c206a6c
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-01-23 11:42:51 +0900

    server to client transmisstion / file-list print end

--- file-transmission/client.py
+++ file-transmission/client.py
@@ -14,12 +14,16 @@ while True:
     print('# 1 클라이언트 -> 서버 업로드')
     print('# 2 서버 -> 클라이언트 다운로드')
     print('# 3 서버 파일 리스트 조회')
+    print('exit -> 999')
     print('menu -> ', end='')
     menu = int(input())
 
     if menu == 1:  # 1 클라이언트 -> 서버 업로드
         pass
     elif menu == 2:  # 2 서버 -> 클라이언트 다운로드
+        menu = str(menu)
+        clientSock.send(menu.encode('utf-8'))
+
         fileName = input('전송할 파일 이름을 입력하시오. : ')
         clientSock.send(fileName.encode('utf-8'))
 
@@ -31,20 +35,31 @@ while True:
             sys.exit()
 
         nowdir = os.getcwd()
+        print('path : ', nowdir)
 
-        with open(nowdir + "\\file-transmission\\" + fileName, "wb") as f:  # 현재 dir에 파일을 받음
+        with open(nowdir + '\\' + fileName, "wb") as f:  # 현재 dir에 파일을 받음
             try:
-                while data:
-                    f.write(data)  # 1024byte 씀
+                while data:  # 데이터가 있을 때까지
+                    f.write(data)  # 1024바이트 쓴다
                     data_transferred += len(data)
-                    data = clientSock.recv(1024)
+
+                    if len(data) < 1024:
+                        break
+                    data = clientSock.recv(1024)  # 1024바이트를 받아 온다
+                print('bye')
             except Exception as ex:
                 print(ex)
 
         print('수신완료 %s, 전송량 %d' % (fileName, data_transferred))
-
-        break
     elif menu == 3:  # 3 서버 파일 리스트 조회
-        pass
+        menu = str(menu)
+        clientSock.send(menu.encode('utf-8'))
+
+        file_list = clientSock.recv(1024)
+        file_list = file_list.decode('utf-8')
+        print('server file-list : ', file_list)
+    elif menu == 999:
+        break
     else:
         print('1 ~ 3의 범위에서 선택해주세요.')
+    print()
